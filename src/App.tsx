99a71e6567ee32bbd2b73360a8d1da3100c27a1f
Jairo Suarez <jairos@blanclabs.com>
2021-08-25 10:17:54 -0500

    feature: Showing reversed list and adding generate and reset button

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { LoremIpsum } from 'lorem-ipsum';
-import React, { useState } from 'react';
+import { FC, useState } from 'react';
 import { InverseList } from './components/InverseList';
 import './App.css';
+import  uniqid  from 'uniqid';
 
 export interface IContent {
   content: string;
+  id: string;
 }
 
-const App: React.FC = () => {
+const App: FC = () => {
   const loremGenerator = new LoremIpsum({
     sentencesPerParagraph: {
       max: 8,
@@ -22,7 +24,9 @@ const App: React.FC = () => {
   const [numberOfItems, setNumberOfItems] = useState<string>('');
   const [list, setList] = useState<IContent[]>([]);
 
-  const deleteListItem = (index: number) => {};
+  const deleteListItem = (index: number) => {
+    setList(list.filter((_, idx) => idx !== index));
+  };
 
   const handleNumberOfItemsChange = (
     e: React.ChangeEvent<HTMLInputElement>
@@ -31,18 +35,25 @@ const App: React.FC = () => {
   };
 
   const generateItems = () => {
-    const items: IContent[] = Array.from(Array(Number(numberOfItems)), () => ({
-      content: loremGenerator.generateParagraphs(1),
-    }));
-    console.log(items);
-    setList(items);
+    const newItems: IContent[] = Array.from(
+      Array(parseInt(numberOfItems, 10)),
+      () => ({
+        content: loremGenerator.generateParagraphs(1),
+        id: uniqid(),
+      })
+    );
+    setList([...list, ...newItems]);
+  };
+
+  const reset = () => {
+    setList([]);
   };
 
   return (
-    <main>
-      <div className="d-flex">
+    <main className="h-screen w-96 flex flex-col">
+      <div className="flex mb-4">
         <input
-          className="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+    className="flex-1 appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:ring-pink-400 border-pink-300 focus:ring-2 focus:border-transparent"
           value={numberOfItems}
           onChange={handleNumberOfItemsChange}
           id="itemNumber"
@@ -51,11 +62,14 @@ const App: React.FC = () => {
         ></input>
         <button
           onClick={generateItems}
-          className="py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-red-500 hover:bg-red-700"
+          className="py-2 px-3 ml-2 font-semibold rounded-lg shadow-md text-white bg-pink-500 hover:bg-pink-700"
         >
           Generate
         </button>
-        <button className="py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-red-500 hover:bg-red-700">
+        <button
+          onClick={reset}
+          className="py-2 px-3 ml-2 font-semibold rounded-lg shadow-md text-white bg-pink-500 hover:bg-pink-700"
+        >
           Reset
         </button>
       </div>
--- src/components/InverseList.tsx
+++ src/components/InverseList.tsx
@@ -1,27 +1,76 @@
-import { Component } from 'react';
-
-export interface IContent {
-  content: string;
-}
-
+import { FC, RefObject, useEffect, useRef, useState } from 'react';
+import { IContent } from '../App';
+import './InverseList.css';
 
 export interface InverseListProps {
   deleteListItem: (index: number) => void;
   list: IContent[];
 }
 
-export interface State {
-  list: [];
-}
+export const InverseList: FC<InverseListProps> = (props: InverseListProps) => {
 
-export class InverseList extends Component<InverseListProps, State> {
-  removeItem = (index: number) => {
-    this.props.deleteListItem(index);
-  }
+  const [showScrollButton, setShowScrollButton] = useState(false);
 
-  render() {
+  const ref: RefObject<HTMLDivElement> = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    ref?.current?.addEventListener("scroll", () => {
+      console.log(ref)
+      if (ref?.current && ref.current?.scrollTop < 0) {
+        setShowScrollButton(true);
+      } else {
+        setShowScrollButton(false);
+      }
+    });
+  }, []);
+
+
+  const scrollToBottom = () => {
+    ref?.current?.scrollTo({ top: 0 });
+  };
+
+  const renderItem = (item: IContent, index: number) => {
+    const newIndex = props.list.length - index - 1;
     return (
-      <h1>LIST:</h1>
-    )
-  }
-}
+      <div
+        key={item.id}
+        className="border mb-2 rounded p-4 bg-gradient-to-r from-indigo-50 to-indigo-100"
+      >
+        <div className="flex justify-between mb-2">
+          <span className="font-bold">
+            item-{newIndex} id:{item.id}
+          </span>
+          <button
+            className="flex justify-center content-center px-2 font-semibold rounded-lg shadow-md text-white bg-pink-500 hover:bg-pink-700"
+            onClick={() => props.deleteListItem(newIndex)}
+          >
+            &#10005;
+          </button>
+        </div>
+        <div className="prose">
+          <p className="text-justify">{item.content}</p>
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <div className="flex flex-col flex-1 overflow-auto">
+      <p>{props.list.length} Items</p>
+      <div>
+        {props.list.length && showScrollButton ? (
+          <a onClick={() => scrollToBottom()} className="">
+            Back to bottom
+          </a>
+        ) : (
+          <span>&nbsp;</span>
+        )}
+      </div>
+      <div
+        ref={ref}
+        className="relative flex-1 overflow-y-scroll flex flex-col-reverse border px-4 py-2 scroll-bottom"
+      >
+        {props.list.slice().reverse().map(renderItem)}
+      </div>
+    </div>
+  );
+};
